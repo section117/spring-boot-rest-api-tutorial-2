9ee25663235cfc166c70f00c463f7de00aa2bab8
SECTION <sec117tion@gmail.com>
2022-05-16 19:36:55 +0530

    Protected endpoints with Keycloak
    - An access token should be obtained from Keycloak Server (Manually)
    - Using that access token, authorization can be tested.
    - Everything works as expected in Spring Authorization Server (Deprecated)

--- src/main/java/com/api/restapitutorial1/security/KeycloakSecurityConfig.java
+++ src/main/java/com/api/restapitutorial1/security/KeycloakSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.api.restapitutorial1.security;
 
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
@@ -12,7 +17,14 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
 
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return null;
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
     @Override
@@ -20,7 +32,7 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         super.configure(http);
         http.authorizeRequests()
                 .anyRequest()
-                .permitAll();
+                .hasAnyRole("admin");
 
     }
 
