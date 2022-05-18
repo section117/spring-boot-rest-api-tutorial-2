ded2db804610dc4a34b310397c1edac5e2935bfc
SECTION <sec117tion@gmail.com>
2022-05-18 18:17:12 +0530

    User registration completed

--- src/main/java/com/api/restapitutorial1/controller/NewsController.java
+++ src/main/java/com/api/restapitutorial1/controller/NewsController.java
@@ -3,10 +3,12 @@ package com.api.restapitutorial1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class NewsController {
     @GetMapping("/news/get")
-    public Object getProtectedNews(){
+    public Object getProtectedNews(Principal principal){
         return "This is a protected news.";
     }
 }
--- src/main/java/com/api/restapitutorial1/service/UserService.java
+++ src/main/java/com/api/restapitutorial1/service/UserService.java
@@ -3,8 +3,11 @@ package com.api.restapitutorial1.service;
 import com.api.restapitutorial1.config.keycloak.KeycloakAdminClientConfig;
 import com.api.restapitutorial1.dao.UserRepository;
 import com.api.restapitutorial1.model.User;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,7 +25,6 @@ public class UserService {
     @Autowired
     private KeycloakAdminClientConfig keycloakAdminClientConfig;
 
-
     @Transactional(rollbackFor = Exception.class)
     public Object addUser(String name, String email, String password, String role) throws Exception {
         User user = new User();
@@ -39,6 +41,7 @@ public class UserService {
         keycloakUser.setFirstName(name);
         keycloakUser.setEmail(email);
         keycloakUser.setEnabled(true);
+        keycloakUser.setEmailVerified(true);
         keycloakUser.setRealmRoles(Collections.singletonList(role));
 
         //Setup password
@@ -49,8 +52,15 @@ public class UserService {
 
         keycloakUser.setCredentials(Collections.singletonList(credentialRepresentation));
 
+        //Save user in Keycloak server
         UsersResource usersResource = keycloakAdminClientConfig.getInstance().users();
         Response response = usersResource.create(keycloakUser);
+
+        //Associate the role with the user
+        RoleRepresentation realmRoleUser = keycloakAdminClientConfig.getInstance().roles().get(role).toRepresentation();
+        String createdId = CreatedResponseUtil.getCreatedId(response);
+        UserResource userResource = usersResource.get(createdId);
+        userResource.roles().realmLevel().add(Collections.singletonList(realmRoleUser));
 
         if(response.getStatus() != 201)
             throw new Exception("Failed to add the user to Keycloak " + response.readEntity(String.class));
